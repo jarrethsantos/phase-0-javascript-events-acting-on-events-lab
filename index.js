c4fa9ae180a17a6c56af2ccc84f279e21c7a8ed6
Jarreth Santos <santos.jarreth@gmail.com>
2024-02-14 12:04:41 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    // You can adjust the limit based on your specific layout
+    const rightLimit = 400;
+
+    if (left < rightLimit) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
